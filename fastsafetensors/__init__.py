b6fea4dd34f92ba7a305f23d1eac791cf95b145a
Takeshi Yoshimura <tyos@jp.ibm.com>
2025-06-06 11:27:16 +0900

    fix __init__.py for lint
    Signed-off-by: Takeshi Yoshimura <tyos@jp.ibm.com>

--- fastsafetensors/__init__.py
+++ fastsafetensors/__init__.py
@@ -1,6 +1,8 @@
 # Copyright 2024 IBM Inc. All rights reserved
 # SPDX-License-Identifier: Apache-2.0
 
-from .loader import SafeTensorsFileLoader, fastsafe_open
-from .common import SafeTensorsMetadata, TensorFrame, SingleGroup, get_device_numa_node, str_to_dtype, alloc_tensor_memory, free_tensor_memory
+from .common import (SafeTensorsMetadata, SingleGroup, TensorFrame,
+                     alloc_tensor_memory, free_tensor_memory,
+                     get_device_numa_node, str_to_dtype)
 from .file_buffer import FilesBufferOnDevice
+from .loader import SafeTensorsFileLoader, fastsafe_open
