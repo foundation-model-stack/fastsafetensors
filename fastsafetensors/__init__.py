37f09eeeb6b865c0c569189c5ff9a1a3961f114f
Takeshi Yoshimura <tyos@jp.ibm.com>
2025-07-18 15:20:19 +0900

    add __version__
    Signed-off-by: Takeshi Yoshimura <tyos@jp.ibm.com>

--- fastsafetensors/__init__.py
+++ fastsafetensors/__init__.py
@@ -1,6 +1,13 @@
 # Copyright 2024 IBM Inc. All rights reserved
 # SPDX-License-Identifier: Apache-2.0
 
+try:
+    from importlib.metadata import version
+except ImportError:  # Python <3.8
+    from importlib_metadata import version
+
+__version__ = version(__name__)
+
 from .common import SafeTensorsMetadata, SingleGroup, TensorFrame, get_device_numa_node
 from .file_buffer import FilesBufferOnDevice
 from .loader import SafeTensorsFileLoader, fastsafe_open
