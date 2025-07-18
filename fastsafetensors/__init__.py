118072033c40aa7938422f16f388cd53e8a65378
Takeshi Yoshimura <tyos@jp.ibm.com>
2025-07-18 15:27:14 +0900

    fix importlib import
    Signed-off-by: Takeshi Yoshimura <tyos@jp.ibm.com>

--- fastsafetensors/__init__.py
+++ fastsafetensors/__init__.py
@@ -1,11 +1,7 @@
 # Copyright 2024 IBM Inc. All rights reserved
 # SPDX-License-Identifier: Apache-2.0
 
-try:
-    from importlib.metadata import version
-except ImportError:  # Python <3.8
-    from importlib_metadata import version
-
+from importlib.metadata import version
 __version__ = version(__name__)
 
 from .common import SafeTensorsMetadata, SingleGroup, TensorFrame, get_device_numa_node
