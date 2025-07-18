d359f5cdf51eaa75791d12940302cfe9ec0fc896
Takeshi Yoshimura <tyos@jp.ibm.com>
2025-07-18 15:28:26 +0900

    format again
    Signed-off-by: Takeshi Yoshimura <tyos@jp.ibm.com>

--- fastsafetensors/__init__.py
+++ fastsafetensors/__init__.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from importlib.metadata import version
+
 __version__ = version(__name__)
 
 from .common import SafeTensorsMetadata, SingleGroup, TensorFrame, get_device_numa_node
