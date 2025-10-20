e6a7e02cc8eb587dffb952ea8dbc86642be7bdea
yuanyuxing.yyx <yuanyuxing.yyx@alibaba-inc.com>
2025-10-20 14:10:44 +0800

    add parallel loader
    Signed-off-by: yuanyuxing.yyx <yuanyuxing.yyx@alibaba-inc.com>

--- fastsafetensors/__init__.py
+++ fastsafetensors/__init__.py
@@ -7,4 +7,5 @@ __version__ = version(__name__)
 
 from .common import SafeTensorsMetadata, SingleGroup, TensorFrame, get_device_numa_node
 from .file_buffer import FilesBufferOnDevice
-from .loader import SafeTensorsFileLoader, fastsafe_open
+from .loader import BaseSafeTensorsFileLoader, SafeTensorsFileLoader, fastsafe_open
+from .parallel_loader import ParallelLoader
