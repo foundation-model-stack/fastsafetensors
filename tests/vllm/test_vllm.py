268425f5bdc731d477a2b3f277c85634589313cb
Takeshi Yoshimura <tyos@jp.ibm.com>
2026-05-12 11:33:29 +0900

    fix ci
    Signed-off-by: Takeshi Yoshimura <tyos@jp.ibm.com>

--- tests/vllm/test_vllm.py
+++ tests/vllm/test_vllm.py
@@ -3,8 +3,6 @@
 import os
 
 import pytest
-
-os.environ["VLLM_WORKER_MULTIPROC_METHOD"] = "spawn"
 import vllm
 from vllm import LLM, SamplingParams
 
@@ -28,11 +26,11 @@ def run_llm(llm):
         print(f"Output: {output.outputs[0].text}")
 
 
-def test_vllm_no_fastsafetensors(fstcpp_log):
+def test_vllm_no_fastsafetensors():
     run_llm(LLM(model="ibm-granite/granite-3.3-2b-instruct", max_model_len=16))
 
 
-def test_vllm_fastsafetensors(fstcpp_log):
+def test_vllm_fastsafetensors():
     run_llm(
         LLM(
             model="ibm-granite/granite-3.3-2b-instruct",
@@ -42,11 +40,23 @@ def test_vllm_fastsafetensors(fstcpp_log):
     )
 
 
-def test_deepseek_r1(fstcpp_log):
+def test_vllm_fastsafetensors_tp():
+    run_llm(
+        LLM(
+            model="ibm-granite/granite-3.3-2b-instruct",
+            load_format="fastsafetensors",
+            max_model_len=16,
+            tensor_parallel_size=2,
+        )
+    )
+
+
+def test_deepseek_r1():
     run_llm(
         LLM(
             model="silence09/DeepSeek-R1-Small-2layers",
             load_format="fastsafetensors",
             max_model_len=16,
+            tensor_parallel_size=2,
         )
     )
